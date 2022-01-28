0cce4ce8e70da1729da345a8c1fed4f43a78bc89
SpaceFish <74864498+SpaceFishGameDev@users.noreply.github.com>
2022-01-28 21:56:36 +0200

    Add project files.

---
+++ OpusDomi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpusDomi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
